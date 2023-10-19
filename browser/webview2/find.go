8850c10438f212235ef3ae3d44f3737c83f0ac73
Crimix <christofferm@live.dk>
2023-10-19 17:40:39 +0200

    Add support for webview2

--- browser/webview2/find.go
+++ browser/webview2/find.go
@@ -1,9 +1,25 @@
 package webview2
 
 import (
+	"github.com/zellyn/kooky/internal/chrome"
 	"github.com/zellyn/kooky/internal/chrome/find"
+	"github.com/zellyn/kooky/internal/cookies"
 )
 
 func FindWebView2CookieStoreFiles(rootsFunc func() ([]string, error)) ([]*find.ChromeCookieStoreFile, error) {
 	return find.FindWebView2CookieStoreFiles(rootsFunc)
+}
+
+func CreateCookieStore(file *find.ChromeCookieStoreFile) *cookies.CookieJar {
+	return &cookies.CookieJar{
+		CookieStore: &chrome.CookieStore{
+			DefaultCookieStore: cookies.DefaultCookieStore{
+				BrowserStr:           file.Browser,
+				ProfileStr:           file.Profile,
+				OSStr:                file.OS,
+				IsDefaultProfileBool: file.IsDefaultProfile,
+				FileNameStr:          file.Path,
+			},
+		},
+	}
 }
